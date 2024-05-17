72a2edfd4dde8f6a9f913939b8876859929d81f8
tobias calzada <toticalzada12@gmail.com>
2024-05-17 12:03:57 -0300

    terminando resposive

--- src/Componentes/Header/PartesDelHeader/Navbar/Navbar.jsx
+++ src/Componentes/Header/PartesDelHeader/Navbar/Navbar.jsx
@@ -1,40 +1,50 @@
 import "./Navbar.css"
+import { useState } from "react"
 
 function Navbar(){
 
-    return(
-        <div className="padreDeNavbar navbar-expand-lg">
-            <nav class="navbar">
-                <div class="container-fluid">
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <div class="collapse navbar-collapse" id="navbarNav">
-                        <ul class="navbar-nav listaDeLinks">
-                            <li class="nav-item">
-                                <a class="linkNavBar" href="#">inicio</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="linkNavBar" href="#idseccion2">certificados</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="linkNavBar" href="#idseccion3">sobre mí</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="linkNavBar" href="#idseccion4">tecnologias</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="linkNavBar" href="#idseccion5">proyectos</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="linkNavBar" href="#idseccion6">redes</a>
-                            </li>
-                        </ul>
-                    </div>
+    const [collapsed, setCollapsed] = useState(true)
+
+    return (
+        <div className="padreDeNavbar">
+            <nav class="navbar navbar-expand-md navbar-dark">
+            <div class="container">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation" onClick={() => setCollapsed(!collapsed)}>
+                    {collapsed ? (<svg xmlns="http://www.w3.org/2000/svg" height="1.8rem" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="white" >
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                    </svg>) : (<svg xmlns="http://www.w3.org/2000/svg" fill="none" height="1.8rem" viewBox="0 0 24 24" strokeWidth={1.5} stroke="white">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+                    </svg>
+                    )}
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav listaDeLinks">
+                        <li class="nav-item">
+                            <a class="linkNavBar" href="#">inicio</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="linkNavBar" href="#idseccion2">certificados</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="linkNavBar" href="#idseccion3">sobre mí</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="linkNavBar" href="#idseccion4">tecnologias</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="linkNavBar" href="#idseccion5">proyectos</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="linkNavBar" href="#idseccion6">redes</a>
+                        </li>
+                    </ul>
                 </div>
-            </nav>
+            </div>
+        </nav>
         </div>
-)
+    )
+}
+
         /*
         <div className="padreDeNavbar">
             <nav className="navBar">
@@ -49,6 +59,6 @@ function Navbar(){
             </nav>
         </div> 
         */
-}
+
 
 export default Navbar;
